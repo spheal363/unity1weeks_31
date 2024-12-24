4feaeb2cea11a291c6c46a5581a1854582f932d7
SumireDoi <m245461@hiroshima-u.ac.jp>
2024-12-24 20:28:38 +0900

    feature: プレイヤーをInput Systeによって移動させる

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private GameInput gameInput;
+
+    private bool isWalking;
+
+    private void Update()
+    {
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+
+        Vector2 moveDir = new Vector2(inputVector.x, inputVector.y);
+        transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
+
+        isWalking = moveDir != Vector2.zero;
+    }
+
+    public bool IsWalking() {
+        return isWalking;
+    }
+}
