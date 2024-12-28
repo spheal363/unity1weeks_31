e213f4e38efb27929afb2b64c0fbed0b266e6dd5
SumireDoi <m245461@hiroshima-u.ac.jp>
2024-12-28 23:05:51 +0900

    fix: プレイヤーをドット絵のアニメーションに変更

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,17 +8,23 @@ public class Player : MonoBehaviour {
     [SerializeField] private GameInput gameInput;
 
     private bool isWalking;
+    private Vector2 moveDir;
 
     private void Update() {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-        Vector2 moveDir = new Vector2(inputVector.x, inputVector.y);
+        moveDir = new Vector2(inputVector.x, inputVector.y);
         transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
 
         isWalking = moveDir != Vector2.zero;
     }
 
-    public bool IsWalking() {
+    public bool getIsWalking() {
         return isWalking;
+    }
+
+    public Vector2 getMoveDir()
+    {
+        return moveDir;
     }
 }
