425819937e5c210c861ea4b7865e4d0b4dc031cc
SumireDoi <m245461@hiroshima-u.ac.jp>
2024-12-29 14:10:31 +0900

    feature: 穴掘り法で迷路データを作成し、オブジェクトを生成しプレイヤーを1マスずつ移動させる

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,27 +4,88 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private GameInput gameInput;
+    [SerializeField] private MapGenerator mapGenerator;
+    [SerializeField] private float moveSpeed = 5f;
 
     private bool isWalking;
+    private bool isMoving;
     private Vector2 moveDir;
+    public Vector2Int currentPos, nextPos;
+
+    public enum DIRECTION {
+        TOP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
+    public DIRECTION direction;
+
+    int[,] move = {
+        { 0, -1 },  // TOP�̏ꍇ
+        { 1, 0 },   // RIGHT�̏ꍇ
+        { 0, 1 },   // DOWN�̏ꍇ
+        { -1, 0 }   // LEFT�̏ꍇ
+    };
+
+    private void Start() {
+        direction = DIRECTION.DOWN;
+        currentPos = new Vector2Int(1, 1);
+        transform.localPosition = mapGenerator.ScreenPos(currentPos);
+    }
 
     private void Update() {
+        if (isMoving) return;
+
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-
         moveDir = new Vector2(inputVector.x, inputVector.y);
-        transform.position += (Vector3)moveDir * moveSpeed * Time.deltaTime;
-
         isWalking = moveDir != Vector2.zero;
+
+        if (isWalking) {
+            _setDirection();
+            StartCoroutine(_move());
+        }
     }
 
     public bool getIsWalking() {
         return isWalking;
     }
 
-    public Vector2 getMoveDir()
-    {
+    public Vector2 getMoveDir() {
         return moveDir;
+    }
+
+    void _setDirection() {
+        if (moveDir.x > 0) {
+            direction = DIRECTION.RIGHT;
+        } else if (moveDir.x < 0) {
+            direction = DIRECTION.LEFT;
+        } else if (moveDir.y > 0) {
+            direction = DIRECTION.TOP;
+        } else if (moveDir.y < 0) {
+            direction = DIRECTION.DOWN;
+        }
+    }
+
+    IEnumerator _move() {
+        isMoving = true;
+        nextPos = currentPos + new Vector2Int(move[(int)direction, 0], move[(int)direction, 1]);
+
+        if (mapGenerator.GetNextMapType(nextPos) != MapGenerator.MAP_TYPE.WALL) {
+            Vector2 startPos = transform.localPosition;
+            Vector2 endPos = mapGenerator.ScreenPos(nextPos);
+            float elapsedTime = 0f;
+
+            while (elapsedTime < 1f / moveSpeed) {
+                transform.localPosition = Vector2.Lerp(startPos, endPos, (elapsedTime * moveSpeed));
+                elapsedTime += Time.deltaTime;
+                yield return null;
+            }
+
+            transform.localPosition = endPos;
+            currentPos = nextPos;
+        }
+
+        isMoving = false;
     }
 }
